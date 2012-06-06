f867ea2297c1001f68f9940bbf2730fe03e42891
Alex Nichol <alexnichol@comcast.net>
2012-06-06 18:12:25 -0400

    Initial Commit

---
+++ AutoConvert/ACAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ACAppDelegate.h
+//  AutoConvert
+//
+//  Created by Alex Nichol on 6/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ACAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ AutoConvert/ACAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  ACAppDelegate.m
+//  AutoConvert
+//
+//  Created by Alex Nichol on 6/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ACAppDelegate.h"
+
+@implementation ACAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
